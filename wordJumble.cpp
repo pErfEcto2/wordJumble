ab2c564a26d816074cf813a31e04c7beb63c6e3c
pErfEcto2 <misha.yaro2004@gmail.com>
2021-09-18 09:19:59 +0300

    Hints added, bugs fixed

--- wordJumble.cpp
+++ wordJumble.cpp
@@ -13,6 +13,24 @@ string outWord;
 int score = 0;
 string toStart;
 bool toContinue;
+int hintCounter = 0;
+
+void printRevealedWord(int num) {
+    cout << "Revealed letters: ";
+        for (int i = 0; i < num; i++) {
+            cout  << srcWord[i];
+        }
+        cout << endl;
+}
+
+void printHint() {
+    if (hintCounter > 3) {
+        printRevealedWord(3);
+    }
+    else if (hintCounter) {
+        printRevealedWord(hintCounter);
+    }
+}
 
 bool sayHi() {
     system("clear");
@@ -20,18 +38,19 @@ bool sayHi() {
     cout << "I'll show you jumbled word, you have to understand what word it is and write it" << endl;
     cout << "Also you have the score, +10 points if your guess was right, -5 if wasn't";
     cout << "(and yes, you can get negative score, it's not a bug, it's the feature)" << endl;
-    cout << "Do you want to try? (y/n): ";
+    cout << "If you were right, but you used hint, to your score would be added 10 - number of hints" << endl;
+    cout << "To show hint write \"hint\"(you can use hints only three times at each game iteration)" << endl;
+    cout << "To exit write \"exit\"" << endl;
+    cout << "Do you want to try? (write \"n\" to exit or something another to start): ";
     cin >> toStart;
-    /*
-    if (toStart == "n") return false;
-    return true;
-    */
     return toStart != "n";
 }
 
 void print() {
     system("clear");
     cout << "Try to find the premordial word(to exit write \"exit\")" << endl;
+    cout << "To show hint write \"hint\"" << endl;
+    printHint();
     cout << "Your score is " << score << endl;
     cout << "The encrypted word is " << outWord << endl;
     cout << "Enter your guess ";
@@ -40,6 +59,7 @@ void print() {
 
 int control() {
     if (input == "exit") return 1; // 1 code to exit
+    else if (input == "hint") hintCounter++;
     else if (input == srcWord) {
         system("clear");
         cout << "You're right!" << endl;
@@ -48,7 +68,8 @@ int control() {
         string yesNo;
         cin >> yesNo;
         if (yesNo == "n") return 1;
-        score = score + 10;
+        score = score + 10 - hintCounter;
+        hintCounter = 0;
         return 2; // 2 code to start new iteration
     }
     return 0; // 0 code to continue
